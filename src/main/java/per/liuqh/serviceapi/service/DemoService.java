99abc51424bb7092da3c0bc58a3ba04571fb97f6
alikiller1 <liu2011321@sina.com>
2017-04-13 20:34:47 +0800

    Signed-off-by: alikiller1 <liu2011321@sina.com>

--- src/main/java/per/liuqh/serviceapi/service/DemoService.java
+++ src/main/java/per/liuqh/serviceapi/service/DemoService.java
@@ -2,17 +2,16 @@ package per.liuqh.serviceapi.service;
 
 import java.util.List;
 
-import per.liuqh.serviceapi.entity.User;
-
+import per.liuqh.serviceapi.entity.User;  
 
 public interface DemoService {  
   
     String sayHello(String name);  
   
-    public List<User> getUsers();  
-    
-    public void test3()throws Exception;
-    
-    public void doTest()throws Exception;
+    public List<User> getUsers();
+
+	void test3() throws Exception;
+
+	void doTest() throws Exception;  
   
 }  
